4666810c2ce574b61e4ddf16069ba60ef9041267
andrea-mangiapia <andrea.mangiapia@gmail.com>
2022-08-26 16:46:21 +0200

    connected database

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Movie;
 
 class HomeController extends Controller
 {
     public function index() {
+        $movies = Movie::all();
+        dd($movies);
+
+
         return view('home');
     }
 }
